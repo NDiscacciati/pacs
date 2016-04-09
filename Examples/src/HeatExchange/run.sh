369c124157228608a982bdc3dda203a507bce522
PACS Student <pacs_student@pacs-virtual-machine.(none)>
2016-04-09 16:14:52 +0200

    Added script and graphical interface Added new parameters file to compare norms

---
+++ Examples/src/HeatExchange/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+make
+printf "Executing the program with L2 norm\n\n"
+./main -p parameters1.pot
+printf "\n\n\n\n\n"
+printf "Executing the program with H1 norm\n\n"
+./main -p parameters2.pot
+printf "\n\n\n\n\n"
+gnuplot myplot.gnu
