808e225c7ad32b5798dddbb910107db555bf15d4
PACS Student <pacs_student@pacs-virtual-machine.(none)>
2016-04-10 09:13:49 +0200

    Removed GS from main.cpp

--- Examples/src/HeatExchange/main.cpp
+++ Examples/src/HeatExchange/main.cpp
@@ -87,53 +87,6 @@ int main(int argc, char** argv)
      theta[m]=(1.-m*h)*(To-Te)/Te;
 
 	status=solve(M,act,toler,itermax,theta,norm); 
-  // Gauss-Seidel
-  // epsilon=||x^{k+1}-x^{k}||
-  // Stopping criteria epsilon<=toler
- /* 
-  int iter=0;
-  double diff=0.;
-  double xnew, epsilon;
-     do
-       { epsilon=0.;
-
-     //First row
-         {   
-	   xnew  = (theta[0]+theta[2])/(2.+h*h*act);
-	   //epsilon += (xnew-theta[1])*(xnew-theta[1]);
-	   epsilon+=h/3*(xnew-theta[1])*(xnew-theta[1]);
-	   diff= xnew-theta[1];
-	   theta[1] = xnew;
-         }
-
-	 //Central elements
-         for(int m=2;m < M;m++)
-         {   
-	   xnew  = (theta[m-1]+theta[m+1])/(2.+h*h*act);
-	   epsilon += h/3*(diff*diff+(xnew-theta[m])*(xnew-theta[m])+diff*(xnew-theta[m]));
-	   diff=xnew-theta[m];
-	   theta[m] = xnew;
-         }
-
-	 //Last row
-	 xnew = theta[M-1]; 
-	 epsilon += h/3*(diff*diff+(xnew-theta[M])*(xnew-theta[M])+diff*(xnew-theta[M]));
-	 theta[M]=  xnew; 
-
-	 cout<<"L2 norm is "<<sqrt(epsilon)<<endl;
-	 iter++;     
-       }while((sqrt(epsilon) > toler) && (iter < itermax) );
-
-    if(iter<itermax)
-      cout << "M="<<M<<"  Convergence in "<<iter<<" iterations"<<endl;
-    else
-      {
-	cerr << "NOT CONVERGING in "<<itermax<<" iterations "<<
-	  "||dx||="<<sqrt(epsilon)<<endl;
-	status=1;
-      }
-
-     */ 
 
  // Analitic solution
 
